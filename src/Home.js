94b817f6acdeb43fa26da5a1c3993a18cd51a294
wagarkowa <victoriaagarkova96@gmail.com>
2020-08-06 19:04:06 +0200

    pobranie postów do consoli

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Heading = styled.h1`
 text-align: center;
+font-family: 'Inconsolata', monospace;
+padding: 40px;
+color: #1A181D;
+font-weight: 500;
 `;
 
-const Home = () => {
-    return (
-        <>
-        <Heading>Home</Heading>
-        </>
-    )
+class Home extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            feeds: []
+        }
+    }
+
+    componentDidMount() {
+        this.getPost();
+    }
+
+    getPost = () => {
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        
+        axios.post(
+            'https://akademia108.pl/api/social-app/post/latest',
+            {'headers': headers})
+            .then((res) => {
+                console.log("RESPONSE RECEIVED: ", res.data);
+
+                let newPost = res.data.map((post) => {
+                    return(
+                        <p>
+                            {res.data.content}
+                        </p>
+                    )
+                })
+            })
+            .catch((err) => {
+                console.log("AXIOS ERROR: ", err);
+            })
+    }
+
+    render() {
+        return (
+            <>
+            <Heading>Home</Heading>
+    
+            <div className="feed">
+                
+            </div>
+            </>
+        )
+    }
 }
 
 export default Home;
