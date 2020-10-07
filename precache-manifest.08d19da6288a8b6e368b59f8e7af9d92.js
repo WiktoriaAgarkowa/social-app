322080be502eaebcdc0403b0dd10eb4a3fe2c6b9
wagarkowa <victoriaagarkova96@gmail.com>
2020-10-07 20:57:00 +0200

    Updates

---
+++ precache-manifest.08d19da6288a8b6e368b59f8e7af9d92.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bd450b2661e6252d3ecba08cef94f3",
+    "url": "/WiktoriaAgarkowa/social-app/index.html"
+  },
+  {
+    "revision": "6f42a693a0b05d7a4d13",
+    "url": "/WiktoriaAgarkowa/social-app/static/css/2.02491d9c.chunk.css"
+  },
+  {
+    "revision": "76e8d8c3cf2d33643b32",
+    "url": "/WiktoriaAgarkowa/social-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f42a693a0b05d7a4d13",
+    "url": "/WiktoriaAgarkowa/social-app/static/js/2.774b9a1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WiktoriaAgarkowa/social-app/static/js/2.774b9a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e8d8c3cf2d33643b32",
+    "url": "/WiktoriaAgarkowa/social-app/static/js/main.3b5ddce5.chunk.js"
+  },
+  {
+    "revision": "8cf2cb220e6df0bfdf7c",
+    "url": "/WiktoriaAgarkowa/social-app/static/js/runtime-main.01641469.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/WiktoriaAgarkowa/social-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/WiktoriaAgarkowa/social-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/WiktoriaAgarkowa/social-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/WiktoriaAgarkowa/social-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/WiktoriaAgarkowa/social-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1e7bd107d934683be9313f8d457d0f33",
+    "url": "/WiktoriaAgarkowa/social-app/static/media/logo.1e7bd107.png"
+  }
+]);
