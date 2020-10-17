0eff259a6ce8dd821180595197626cda20562938
wagarkowa <victoriaagarkova96@gmail.com>
2020-10-17 17:25:04 +0200

    Updates

---
+++ precache-manifest.fbd4a6d67643983231c8eee89c5afa21.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acec5acc292911ee99b5aed97f358a5b",
+    "url": "/social-app/index.html"
+  },
+  {
+    "revision": "3c598f51a8ad45c9bc7b",
+    "url": "/social-app/static/css/2.4f2c5198.chunk.css"
+  },
+  {
+    "revision": "b609927fb801d8b0b8cb",
+    "url": "/social-app/static/css/main.ae8ab0c7.chunk.css"
+  },
+  {
+    "revision": "3c598f51a8ad45c9bc7b",
+    "url": "/social-app/static/js/2.b62bb70f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-app/static/js/2.b62bb70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b609927fb801d8b0b8cb",
+    "url": "/social-app/static/js/main.ded04ae5.chunk.js"
+  },
+  {
+    "revision": "25e0322c91d9d4c0938f",
+    "url": "/social-app/static/js/runtime-main.d9647911.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/social-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/social-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/social-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/social-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/social-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "769824e7b70019956886fc52c41ff6ac",
+    "url": "/social-app/static/media/logo-min.769824e7.png"
+  },
+  {
+    "revision": "45e9989c96106a380eac70a41e1097c0",
+    "url": "/social-app/static/media/logo.45e9989c.png"
+  }
+]);
